b0126e7f62301aa5d2ccb07abe1461a9b9ff06b1
kobucyacya <i_ran_i_ran@icloud.com>
2024-08-06 00:03:24 +0900

    Add files via upload

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -1,9 +1,11 @@
 //btnを取得して代入
 const btn = document.getElementById('btn');
 //クリックして２秒後に実行
-setTimeout(() => {
+btn.addEventListener('click', () => { 
+  setTimeout(() => {
     //textを取得して代入
     const text = document.getElementById('text');
     //テキストを追加
     text.textContent ='ボタンをクリックしました';
   }, 2000);
+});
